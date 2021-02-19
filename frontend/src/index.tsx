4569ef2c5e71726f4b2bb1bd176481ea6122e400
Aimee <chiehhan.s17@gmail.com>
2021-02-19 17:48:02 +0100

    init template

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import * as React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import StyledEngineProvider from '@material-ui/core/StyledEngineProvider';
+import { ThemeProvider } from '@material-ui/core/styles';
 import App from './App';
+import theme from './theme';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  // TODO v5: remove once migration to emotion is completed
+  <StyledEngineProvider injectFirst>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </StyledEngineProvider>,
+  document.querySelector('#root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
