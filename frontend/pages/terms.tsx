1e9f6600009abd86a7dd50c6f86142b133fda081
lenatech <chiehhan.s17@gmail.com>
2021-04-13 17:42:44 +0200

    some temporarily data

---
+++ frontend/pages/terms.tsx
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Container from '@material-ui/core/Container';
+import Box from '@material-ui/core/Box';
+import Markdown from '../src/components/Markdown';
+import Typography from '../src/components/Typography';
+import AppAppBar from '../src/views/App/AppAppBar';
+import AppFooter from '../src/views/App/AppFooter';
+import withRoot from '../src/withRoot';
+import terms from '../src/views/markdowns/terms.md';
+
+function Terms() {
+  return (
+    <React.Fragment>
+      <AppAppBar />
+      <Container>
+        <Box mt={7} mb={12}>
+          <Typography variant="h3" gutterBottom marked="center" align="center">
+            Terms
+          </Typography>
+          <Markdown>{terms}</Markdown>
+        </Box>
+      </Container>
+      <AppFooter />
+    </React.Fragment>
+  );
+}
+
+export default withRoot(Terms);
