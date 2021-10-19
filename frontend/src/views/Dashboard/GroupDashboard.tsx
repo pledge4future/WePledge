ccdad979ec38a1b6f4e798d2091392d9bdb042c2
Fabian Kneissl <fkneissl@appointrix.de>
2021-10-19 15:11:39 +0200

    adjust demo dashboards

--- frontend/src/views/Dashboard/GroupDashboard.tsx
+++ frontend/src/views/Dashboard/GroupDashboard.tsx
@@ -1,5 +1,80 @@
+import { makeStyles } from "@material-ui/core";
+import { ChartColors } from './viz/VizColors';
+import React, { useState, useCallback } from "react";
+import { ComposedChart, Bar, XAxis, YAxis, Tooltip, Line } from 'recharts';
+
+import { CustomLegend, CustomLegendItem } from './viz/Charts/ReCharts/CustomLegend';
+import { getAllExampleData } from "../../../static/demo/demoDataGenerator";
+
+const useStyles = makeStyles({
+  legendContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginLeft: '200px'
+  },
+})
+
+
+
 export default function GroupDashboard(){
-  return(
-    <div>Group Emissions</div>
-  );
+
+  const styles = useStyles();
+
+  const [showElectricity, setShowElectricity] = useState(true);
+  const [showHeating, setShowHeating] = useState(true);
+  const [showCommuting, setShowCommuting] = useState(true);
+  const [showBusiness, setShowBusiness] = useState(true);
+
+  const legendData: CustomLegendItem[] = [
+    { label: 'Electricity', color: ChartColors.electricity, shown: showElectricity, onItemChange: (() => setShowElectricity(!showElectricity))  },
+    { label: 'Commuting', color: ChartColors.commuting, shown: showCommuting, onItemChange: (() => setShowCommuting(!showCommuting)) },
+    { label: 'Heating', color: ChartColors.heating, shown: showHeating, onItemChange: (() => setShowHeating(!showHeating)) },
+    { label: 'Business', color: ChartColors.business, shown: showBusiness, onItemChange: (() => setShowBusiness(!showBusiness)) },
+    /* { label: 'per Capita',color: ChartColors.perCapitaLine},
+    { label: 'Trendline', color: ChartColors.trendLine}, */
+  ]
+
+  const exampleData = getAllExampleData(10);
+  
+  const renderComposedGroupChart = useCallback(() => {
+    return (
+      <div>
+      <ComposedChart width={1000} height={400} data={exampleData}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        ({ 
+        showElectricity && <Bar dataKey="electricity" barSize={20} fill={ChartColors.electricity} stackId="a" />
+        })
+        ({
+          showHeating && <Bar dataKey="heating" barSize={20} fill={ChartColors.heating} stackId="a" />
+        })
+        ({
+          showCommuting && <Bar dataKey="commuting" barSize={20} fill={ChartColors.commuting} stackId="a" />
+        })
+        ({
+        showBusiness && <Bar dataKey="business" barSize={20} fill={ChartColors.business} stackId="a" />
+        })
+        <Line dataKey="sum" stroke={ChartColors.trendLine} />
+        <Line dataKey="max" stroke={ChartColors.perCapitaLine} />
+      </ComposedChart>
+      <div className={styles.legendContainer}>
+        <CustomLegend items = {legendData}/>
+      </div>
+      </div>
+
+    )
+  }, [showElectricity, showHeating, showCommuting, showBusiness]);
+  
+  return (
+    <React.Fragment>
+    <h3>Group Emissions</h3>
+    <div id="ChartContainer">
+      {
+        renderComposedGroupChart()
+      }
+    </div>
+    </React.Fragment>
+  )
 }
--- frontend/src/views/Dashboard/IndividualDashboard.tsx
+++ frontend/src/views/Dashboard/IndividualDashboard.tsx
@@ -4,41 +4,7 @@ import { ChartColors } from './viz/VizColors';
 import { CustomLegend, CustomLegendItem } from './viz/Charts/ReCharts/CustomLegend';
 
 import { makeStyles } from '@material-ui/core/styles';
-
-function getRandomEmissions(){
-  return Math.floor(Math.random() * 500) + 100;
-}
-
-function getRandomData(month: string){
-
-  const data = [getRandomEmissions(),getRandomEmissions(),getRandomEmissions(),getRandomEmissions()]
-
-  return {
-    "name": month,
-    "electricity": data[0],
-    "heating": data[1],
-    "commuting": data[2],
-    "business": data[3],
-    "sum": data.reduce((total, current) => total += current),
-    "max": 1000
-  }
-}
-
-
-const exampleData = [
-    getRandomData("Januar"),
-    getRandomData("Feburar"),
-    getRandomData("März"),
-    getRandomData("April"),
-    getRandomData("Mai"),
-    getRandomData("Juni"),
-    getRandomData("Juli"),
-    getRandomData("August"),
-    getRandomData("September"),
-    getRandomData("Oktober"),
-    getRandomData("November"),
-    getRandomData("Dezember"),
-]
+import { getAllExampleData } from "../../../static/demo/demoDataGenerator";
 
 const useStyles = makeStyles({
   legendContainer: {
@@ -59,6 +25,7 @@ export function IndividualDashboard(){
   const [showCommuting, setShowCommuting] = useState(true);
   const [showBusiness, setShowBusiness] = useState(true);
 
+
   const legendData: CustomLegendItem[] = [
     { label: 'Electricity', color: ChartColors.electricity, shown: showElectricity, onItemChange: (() => setShowElectricity(!showElectricity))  },
     { label: 'Commuting', color: ChartColors.commuting, shown: showCommuting, onItemChange: (() => setShowCommuting(!showCommuting)) },
@@ -67,6 +34,8 @@ export function IndividualDashboard(){
     /* { label: 'per Capita',color: ChartColors.perCapitaLine},
     { label: 'Trendline', color: ChartColors.trendLine}, */
   ]
+
+  const exampleData = getAllExampleData();
 
   const renderComposedChart = useCallback(() => {
     return (
