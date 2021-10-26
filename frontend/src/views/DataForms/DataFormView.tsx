164da3f7913883cc7b92c15ae6d8a065a32d90d6
Fabian Kneissl <fkneissl@appointrix.de>
2021-10-26 10:29:40 +0200

    add overview for data forms

--- frontend/src/views/DataForms/DataFormView.tsx
+++ frontend/src/views/DataForms/DataFormView.tsx
@@ -1,9 +1,42 @@
-import React from "react";
+import { Tab, Tabs } from "@material-ui/core";
+import React, { useState } from "react";
+import { TabPanel } from "../Dashboard/TabPanel";
 
 import { ElectricityForm } from './ElectricityForm';
 
+function a11yProps(index: any){
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpane.l${index}`
+  }
+}
+
+
 export default function DataFormView(){
+
+  const [value, setValue] = useState(0)
+
+  const handleChange = (event: React.ChangeEvent<{}>, newValue: number)=>{
+    setValue(newValue);
+  }
+
+  const tabContents = [<ElectricityForm />, <span>Insert heating form</span>, <span>Insert commuting form</span>, <span>insert business trip form</span>]
+
   return <React.Fragment>
-    <ElectricityForm></ElectricityForm>
+    <Tabs value={value} onChange={handleChange} aria-label="forms tabs">
+      <Tab label="Electricity" {...a11yProps(0)} />
+      <Tab label="Heating" {...a11yProps(1)} />
+      <Tab label="Commuting" {...a11yProps(2)} />
+      <Tab label="Business Trip" {...a11yProps(3)} />
+    </Tabs>
+    {
+      tabContents.map((content, index) => {
+        return (
+          <TabPanel value={value} index={index}>
+            {content}
+          </TabPanel>
+        );
+      })
+    }
   </React.Fragment>
 }
