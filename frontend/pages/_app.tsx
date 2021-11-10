448fa309f249de34a52ee7de4f00f9ecd7127a32
Fabian Kneissl <fkneissl@appointrix.de>
2021-11-10 15:26:39 +0100

    add basic google analytics setup

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { AppProps } from "next/app";
 
 // Theme
@@ -11,9 +11,30 @@ import { ApolloProvider } from "@apollo/client";
 import client from "../src/api/apollo-client";
 
 import { AuthContextProvider } from "../src/providers/Auth";
+import { useRouter } from "next/router";
+
+import * as ga from '../lib/ga';
 
 export default function MyApp(props: AppProps) {
   const { Component, pageProps } = props;
+
+  const router = useRouter()
+
+  // used for google analytics
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      ga.pageview(url)
+    }
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
 
   React.useEffect(() => {
     // Remove the server-side injected CSS.
