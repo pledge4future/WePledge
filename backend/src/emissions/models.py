d56e0ce244d23fe6400d093c69cba77e35549005
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2022-08-06 18:42:15 +0200

    set is_representative as readonly in django admin

--- backend/src/emissions/models.py
+++ backend/src/emissions/models.py
@@ -10,7 +10,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db.models import Sum
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
-
 from co2calculator.co2calculator import (
     CommutingTransportationMode,
     BusinessTripTransportationMode,
