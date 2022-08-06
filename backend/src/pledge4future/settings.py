1007e6d6e95afae744af40dfcbf90f536276a67b
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2022-08-06 18:41:30 +0200

    added logging to settings.py

--- backend/src/pledge4future/settings.py
+++ backend/src/pledge4future/settings.py
@@ -205,3 +205,17 @@ GRAPH_MODELS = {
     "all_applications": True,
     "group_models": True,
 }
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+}
