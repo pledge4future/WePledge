23d25dbddac092f0e4f582ea64c490b7d7f47e38
codingfabi <fabian.kneissl@gmx.de>
2022-08-17 22:53:57 +0200

    add basic emission estimation view

---
+++ frontend/src/views/EmissionEstimation/EmissionEstimationView.tsx
@@ -0,0 +1,19 @@
+import { Grid } from "@material-ui/core";
+import React from "react";
+
+
+
+
+export default function EmissionEstimationView(){
+
+
+
+  return(
+  <React.Fragment>
+      <Grid container>
+        <div>enter input here</div>
+    </Grid>
+  </React.Fragment>
+  );
+}
+
