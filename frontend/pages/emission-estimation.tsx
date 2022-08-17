f5cd7f6c60163307f4ad524d68d1fd7e4adbf584
Fabian Kneissl <fabian.kneissl@sap.com>
2022-08-17 22:53:57 +0200

    add basic emission estimation view

---
+++ frontend/pages/emission-estimation.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import withRoot from "../src/withRoot";
+
+import PageContainer from "../src/components/PageContainer";
+import EmissionEstimation from "../src/views/EmissionEstimation";
+
+function DashboardPage() {
+  return (
+    <PageContainer title={"EmissionEstimation"} showTitle={true}>
+      <EmissionEstimation />
+    </PageContainer>
+  );
+}
+
+export default withRoot(DashboardPage);
