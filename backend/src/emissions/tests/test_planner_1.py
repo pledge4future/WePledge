02955cd9a05d87f4c1c56921533cef39bd1a3e8d
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2022-11-13 11:50:15 +0100

    fixed value error, trip planner endpoint tests running

--- backend/src/emissions/tests/test_planner_1.py
+++ backend/src/emissions/tests/test_planner_1.py
@@ -23,21 +23,20 @@ logger.info(GRAPHQL_URL)
 
 @pytest.mark.parametrize('transportation_mode', ['Car'])
 @pytest.mark.parametrize('size', ['small', 'medium', 'large', 'average'])
-@pytest.mark.parametrize('fuel_type', ['diesel', 'gasoline', 'cng', 'electricity', 'hybrid', 'plug-in_hybrid', 'average'])
-@pytest.mark.parametrize('passengers', list(range(1,10)))
-@pytest.mark.parametrize('round_trip', [True, False])
+@pytest.mark.parametrize('fuel_type', ['diesel', 'gasoline', 'cng', 'electric', 'hybrid', 'plug-in_hybrid', 'average'])
+@pytest.mark.parametrize('passengers', list(range(1,4)))
+@pytest.mark.parametrize('round_trip', [False])
 def test_plan_trip_car(transportation_mode, size, fuel_type, passengers, round_trip):
     """Test whether trip planner throws error for different parameter combinations"""
     query = """
-        mutation planTrip ($transportationMode: String!, $size: String!, $fuelType: String!, $passengers: Integer!, $roundTrip: Boolean!) {
+        mutation planTrip ($transportationMode: String!, $size: String!, $fuelType: String!, $passengers: Int!, $roundTrip: Boolean!) {
             planTrip (input: {
                 transportationMode: $transportationMode
                 distance: 200
-                stops: []
                 size: $size
                 fuelType: $fuelType
                 passengers: $passengers
-                roundtTrip: $roundTrip
+                roundtrip: $roundTrip
             }) {
                 success
                 message
@@ -49,11 +48,11 @@ def test_plan_trip_car(transportation_mode, size, fuel_type, passengers, round_t
     headers = {
         "Content-Type": "application/json",
     }
-    variables = {"transportationMode": transportation_mode, "size": size, "fueltype": fueltype, "passengers": passengers, "roundTrip": round_trip}
+    variables = {"transportationMode": transportation_mode, "size": size, "fuelType": fuel_type, "passengers": passengers, "roundTrip": round_trip}
     response = requests.post(GRAPHQL_URL, json={"query": query, "variables": variables}, headers=headers)
     logger.warning(response.content)
     assert response.status_code == 200
     data = response.json()
     logger.warning(data)
-    assert data["data"]["planTrip"]["success"] == "True"
+    assert data["data"]["planTrip"]["success"]
 
