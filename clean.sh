3555715cb8131b64beb17560d4b229494e14bcfc
Christina Ludwig <christina.ludwig@uni-heidelberg.de>
2022-11-20 16:21:00 +0100

    add deletion of volumens, containers and images to clean.sh

--- clean.sh
+++ clean.sh
@@ -1,1 +1,5 @@
+docker compose down
+docker volume rm wepledge_backend_env wepledge_backend_extensions wepledge_db-data wepledge_frontend_extensions wepledge_frontend_nodemodules
+docker image rm wepledge-backend wepledge-frontend
+rm -Rf ./backend/src/emissions/migrations/0*.py
 rm -Rf ./**/**/__pycache__/
