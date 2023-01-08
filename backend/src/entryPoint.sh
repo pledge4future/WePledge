45fab8478a78f2e0d1eb0f3a7958bddcbaab8dc6
christina <christina.ludwig@uni-heidelberg.de>
2023-01-08 12:46:56 +0100

    renamed create_test_users.py to create_test_data.py

--- backend/src/entryPoint.sh
+++ backend/src/entryPoint.sh
@@ -4,7 +4,7 @@ python manage.py makemigrations emissions
 python manage.py migrate
 python manage.py load_institutions
 python manage.py loaddata research_fields.json
-python manage.py create_test_users
+python manage.py create_test_data
 python manage.py graph_models emissions -a -o /home/python/app/assets/database_structure.png --pydot
 #python manage.py populate_data
 python manage.py collectstatic --noinput
