d38157a122d59ae00825f3e5f6a97ac75d0dca0a
sami1riaz <sami1riaz@gmail.com>
2023-01-29 18:32:34 -0500

    Trying to make submit form work

---
+++ frontend/src/views/DataForms/FormModels.ts
@@ -0,0 +1,27 @@
+const months = [
+  { label: "1", value: 0},
+  { label: "2", value: 1},
+  { label: "3", value: 2},
+  { label: "4", value: 3},
+  { label: "5", value: 4},
+  { label: "6", value: 5},
+  { label: "7", value: 6},
+  { label: "8", value: 7},
+  { label: "9", value: 8},
+  { label: "10", value: 9},
+  { label: "11", value: 10},
+  { label: "12", value: 11}
+]
+
+const years = [
+  { label: "2019", value: 2019},
+  { label: "2020", value: 2020},
+  { label: "2021", value: 2021},
+  { label: "2022", value: 2022},
+  { label: "2023", value: 2023}
+]
+
+export {
+	months,
+	years
+}
---
+++ frontend/src/views/DataForms/FormSubComponents/index.ts
@@ -0,0 +1,9 @@
+import { FormTextField } from "./FormTextField";
+import { FormSelectField } from "./FormSelectField";
+import { InputFieldTooltip } from "./InputFieldTooltip";
+
+export {
+	FormTextField,
+	FormSelectField,
+	InputFieldTooltip
+}
