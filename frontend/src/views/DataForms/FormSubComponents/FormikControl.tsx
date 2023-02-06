d20ee90b36f992b4f5c8c46c3193b035610b6d8f
sami1riaz <sami1riaz@gmail.com>
2023-02-06 18:40:11 -0500

    Style Textarea component

--- frontend/src/views/DataForms/FormSubComponents/FormikControl.tsx
+++ frontend/src/views/DataForms/FormSubComponents/FormikControl.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
+import {Input, Textarea, Select, } from '.';
 
 function FormikControl(props) {
-	const {control} = props
+	const {control, ...rest} = props
 	switch(control) {
-		case 'input':
-		case 'textarea':
-		case 'select':
-		case 'radio':
-		case 'checkbox':
-		case 'date':
+		case 'input': return <Input {...rest}/>
+		case 'textarea': return <Textarea {...rest}/>
+		case 'select': return <Select {...rest}/>
+		// case 'radio': return <Input {...rest}/>
+		// case 'checkbox': return <Input {...rest}/>
+		// case 'date': return <Input {...rest}/>
 		default: return null
 	}
-	return (
-		<div>
-
-		</div>
-	)
-
 }
 
 export default FormikControl
--- frontend/src/views/DataForms/FormSubComponents/Textarea.tsx
+++ frontend/src/views/DataForms/FormSubComponents/Textarea.tsx
@@ -1,18 +1,34 @@
-import React from 'react'
-import { Field, ErrorMessage } from 'formik'
-import {TextField } from '@material-ui/core';
+import React from "react";
+import { Field, ErrorMessage } from "formik";
+import { InputFieldTooltip } from "./InputFieldTooltip";
+import { tooltips } from "../FormTooltips";
+import { TextField } from "@material-ui/core";
 
 // import TextError from './TextError'
 
-function Textarea (props) {
-  const { label, name, ...rest } = props
+function Textarea(props) {
+  const { label, name, ...rest } = props;
   return (
-    <div className='form-control'>
+    <div className="form-control">
       <label htmlFor={name}>{label}</label>
-      <TextField id={name} name={name} {...rest} />
+      <TextField
+        id={name}
+        name={name}
+        {...rest}
+        style={{ margin: 8 }}
+        fullWidth
+        margin="normal"
+        InputLabelProps={{
+          shrink: true,
+        }}
+        variant="outlined"
+        InputProps={{
+          endAdornment: <InputFieldTooltip position={"end"} text={tooltips.building} />,
+        }}
+      />
       {/* <ErrorMessage component={TextError} name={name} /> */}
     </div>
-  )
+  );
 }
 
-export default Textarea
+export default Textarea;
